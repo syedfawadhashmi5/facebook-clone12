6be5d1b7fa2f00976762669945dc6c015524f930
Syed Fawad Hashmi <59658569+syedfawadhashmi5@users.noreply.github.com>
2023-06-06 17:47:12 +0500

    asasas

--- data.js
+++ data.js
@@ -1,19 +1,22 @@
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAseB0RmlDTbt8O5VG7Qc3CAUwxF_TY9Nw",
-    authDomain: "ab-school-52f07.firebaseapp.com",
-    databaseURL: "https://ab-school-52f07.firebaseio.com",
-    projectId: "ab-school-52f07",
-    storageBucket: "ab-school-52f07.appspot.com",
-    messagingSenderId: "1046171942187",
-    appId: "1:1046171942187:web:4b13d89f2c5f0948d5196e",
-    measurementId: "G-2BF832P6N9"
+    apiKey: "AIzaSyBcS9ZzEkaHduKbpHYAxP_MNbOeg3PoCio",
+    authDomain: "demodeta-5a615.firebaseapp.com",
+    projectId: "demodeta-5a615",
+    storageBucket: "demodeta-5a615.appspot.com",
+    messagingSenderId: "56079100568",
+    appId: "1:56079100568:web:6fe6c85cf1a84cd311feed",
+    measurementId: "G-8M9ZNT2TDT"
   };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// Get a reference to the database service
+var database = firebase.database();
 
+// Get a reference to the database node for email and password
+var usersRef = database.ref("users");
 
 // Function to handle user registration
 function UserRegister(){
@@ -29,3 +32,31 @@ function UserRegister(){
             alert("Error signing up");
         });
 }
+
+// Function to handle user sign-in
+function SignIn(){
+    var email = document.getElementById('eemail').value;
+    var password = document.getElementById('lpassword').value;
+    // Check if the email and password match any user in the database
+    usersRef.orderByChild("email").equalTo(email).once("value", function(snapshot) {
+        if (snapshot.exists()) {
+            snapshot.forEach(function(childSnapshot) {
+                var childData = childSnapshot.val();
+                if (childData.password === password) {
+                    // If the email and password match, redirect the user to Google
+                    window.open("https://www.google.com","_self");
+                } else {
+                    alert("Incorrect password");
+                }
+            });
+        } else {
+            alert("User does not exist");
+        }
+    });
+}
+
+// Add event listener to the form submit button
+document.getElementById('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    UserRegister();
+});
