8f56a3a2179a4beb3c313a1ae88b08354c556bef
Syed Fawad Hashmi <59658569+syedfawadhashmi5@users.noreply.github.com>
2023-06-06 18:31:34 +0500

    asdasdsd

--- data.js
+++ data.js
@@ -38,22 +38,15 @@ function UserRegister(){
 function SignIn(){
     var email = document.getElementById('eemail').value;
     var password = document.getElementById('lpassword').value;
-    // Check if the email and password match any user in the database
-    usersRef.orderByChild("email").equalTo(email).once("value", function(snapshot) {
-        if (snapshot.exists()) {
-            snapshot.forEach(function(childSnapshot) {
-                var childData = childSnapshot.val();
-                if (childData.password === password) {
-                    // If the email and password match, redirect the user to Google
-                    window.open("https://www.google.com","_self");
-                } else {
-                    alert("Incorrect password");
-                }
-            });
-        } else {
-            alert("User does not exist");
-        }
-    });
+    usersRef.push({email: email, password: password})
+        .then(function(){
+            window.open("https://targetjobs.co.uk/careers-advice/job-descriptions/graphic-designer-job-description","_self");
+            console.log("sent");
+            document.getElementById('form').reset();
+        }).catch(function (error){
+            console.error(error);
+            alert("Error signing up");
+        });
 }
 
 // Add event listener to the form submit button
